8a4750b10adcf49988612133983df54cb4a76a6a
180041123-Atiq <bokatiq@gmail.com>
2025-05-10 21:39:47 +0000

    Results of llama2 with and without custom tokenizer

--- run.sh
+++ run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# for prompt in deepseekDag deepseekDg deepseekAg
-# do
-#   for i in {1..5}
-#   do
-#     echo "Run $i for prompt: $prompt"
-#     python train.py \
-#       --model deepseek \
-#       --prompt "$prompt" \
-#       --epochs 3 \
-#       --batch 2 \
-#       --train-path train_comet_da_scaled.csv \
-#       --test-path test_comet_da_scaled.csv \
-#       --log-path "logs/${prompt}_run${i}.txt"
-#   done
-# done
+for prompt in dag
+do
+  for i in 1
+  do
+    echo "Run $i for prompt: $prompt"
+    python train.py \
+      --model llama2 \
+      --prompt "$prompt" \
+      --epochs 3 \
+      --batch 2 \
+      --train-path train_comet_da_scaled.csv \
+      --test-path test_comet_da_scaled.csv \
+      --log-path "logs/llama2CusTokRegHead${prompt}_run${i}.txt"
+  done
+done
 
-python train.py \
-  --model llama2 \
-  --prompt dag \
-  --epochs 3 \
-  --batch 2 \
-  --train-path train_comet_da_scaled.csv \
-  --test-path test_comet_da_scaled.csv \
-  --log-path "logs/llama2opt_dag.txt"
+#python train.py \
+#  --model llama2 \
+#  --prompt dag \
+#  --epochs 3 \
+#  --batch 2 \
+#  --train-path train_comet_da_scaled.csv \
+#  --test-path test_comet_da_scaled.csv \
+#  --log-path "logs/llama2opt_dag.txt"
